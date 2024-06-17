38d440a2a66e1add429e202b528aa82d65ee9ee9
Aleksandr Aleksandrov <mortyleano@MacBook-Pro-Aleksandr.local>
2024-06-18 02:23:58 +0300

    #8: some fixes

--- src/test/java/homework_3/CheckPracticeFormTest.java
+++ src/test/java/homework_3/CheckPracticeFormTest.java
@@ -1,10 +1,10 @@
 package homework_3;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -35,14 +35,13 @@ public class CheckPracticeFormTest {
         $("#genterWrapper").$(byText("Male")).click();
         $("#userNumber").setValue("0123456789");
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOptionByValue("9");
-        $(".react-datepicker__year-select").selectOptionByValue("1996");
-        $(".react-datepicker__day--009").click();
+        $(".react-datepicker__month-select").selectOption("September");
+        $(".react-datepicker__year-select").selectOption("1996");
+        $(".react-datepicker__day--009:not(.react-datepicker__day--outside-month)").click();
         $(".practice-form-wrapper").click();
         $("#subjectsInput").setValue("Computer Science").pressEnter();
         $("#hobbiesWrapper").$(byText("Music")).click();
         $("#uploadPicture").uploadFromClasspath("Wood.jpg");
-        $("#stateCity-wrapper").scrollTo();
         $("#currentAddress").setValue("Aleksandrov street");
         $("#state").click();
         $("#stateCity-wrapper").$(byText("Haryana")).click();
@@ -53,17 +52,18 @@ public class CheckPracticeFormTest {
         /*
           Проверяем полученные результаты после отправки формы
          */
+        $(".modal-dialog").should(appear);
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
         $(".table-responsive").shouldHave(
-                Condition.text("Student Name Aleksandr Aleksandrov"),
-                Condition.text("Student Email aleksandrov@aleks.com"),
-                Condition.text("Gender Male"),
-                Condition.text("Mobile 0123456789"),
-                Condition.text("Date of Birth 09 October,1996"),
-                Condition.text("Subjects Computer Science"),
-                Condition.text("Hobbies Music"),
-                Condition.text("Picture Wood.jpg"),
-                Condition.text("Address Aleksandrov street"),
-                Condition.text("State and City Haryana Karnal"));
+                text("Student Name Aleksandr Aleksandrov"),
+                text("Student Email aleksandrov@aleks.com"),
+                text("Gender Male"),
+                text("Mobile 0123456789"),
+                text("Date of Birth 09 September,1996"),
+                text("Subjects Computer Science"),
+                text("Hobbies Music"),
+                text("Picture Wood.jpg"),
+                text("Address Aleksandrov street"),
+                text("State and City Haryana Karnal"));
     }
 }
